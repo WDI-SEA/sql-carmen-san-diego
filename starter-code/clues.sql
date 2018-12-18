d8457c2bf8eb588a01d1508dc14863515650fa08
quiwaan <quiwaan@me.com>
2018-12-17 20:44:50 -0800

    found her hiding

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,6 +2,10 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+SELECT * FROM country WHERE region='Southern Europe' ORDER BY population LIMIT 1;
+
+VATICAN
+
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
@@ -9,10 +13,21 @@
 -- spoken in this country, so we can call in a translator to work with you.
 
 
+SELECT language FROM countrylauguage WHERE countrycode='VAT';
+
+ITALIAN
+
+
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+
+
+SELECT * FROM countrylauguage WHERE language='italian' AND percentage=100;
+
+countrycode SMR
+
 
 
 
@@ -22,16 +37,24 @@
  --  be flying to.
 
 
+ SELECT * FROM city WHERE countrycode='SMR' AND name <> 'San Marino';
+
+
+
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+
+SELECT * FROM city WHERE NAME LIKE 'Serra%'  SELECT * FROM country WHERE code='BRA'; serra BRA
 
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+
+ SELECT capital from COUNTRY WHERE code='BRA' SELECT name FROM city WHERE id=211;
 
 
 
@@ -48,10 +71,11 @@
 -- I need a little more sunshine with my slice of life.
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
+SELECT name FROM city Where population=91084;
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
 
--- She's in ____________________________!
+-- She's in ______santa monica ______________________!
