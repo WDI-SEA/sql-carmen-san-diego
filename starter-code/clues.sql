61b80df6e7674e06b46c8eb796fbc549a150b588
karlmiche <karlymism@gmail.com>
2020-06-18 13:11:25 -0700

    correct search terms from King demo added to clue #6

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -47,7 +47,9 @@ world=# SELECT name FROM cities WHERE countrycode = 'BRA';
  -- follow right behind you!
 world=# SELECT * FROM countries where name = 'Brazil';
 --She is in Brasilia!
-
+--Sarah notes:
+SELECT capital FROM countries WHERE code = 'BRA';
+SELECT name FROM cities WHERE id =211;
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
