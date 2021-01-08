4ea905b1717756a8afff9e090d1942f18b8dee04
Your Name <you@example.com>
2021-01-07 20:32:44 -0800

    Figured out clues 2 and 3.

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -4,22 +4,37 @@
 
 SELECT * FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC;
 
-Carmen was spotted in VAT 
+-- Carmen was spotted in VAT (Vatican City)
 
-
+------------------------------------------------------------------
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
-SELECT * FROM countrylanguages WHERE
+SELECT * FROM countrylanguages WHERE countrycode = 'VAT'; 
+-- I got errors when I used double quotes. HAVE to use single quotes in SQL!!
+-- Official language in VAT is Italian.
 
-
+-------------------------------------------------------------------
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+-- Want to do an inner join where countries.region = Southern Europe, countrylanguages.language = Italian, countrylanguages.isofficial = t. Foreign key is countrycode.
 
+SELECT c.code, c.region, cl.language, cl.isofficial
+FROM countries c INNER JOIN countrylanguages cl
+ON c.code=cl.countrycode
+WHERE c.region = 'Southern Europe' AND cl.language = 'Italian' AND cl.isofficial = 't'
+-- Carmen is either in ITA or SMR, and is leaving VAT.
 
+SELECT * FROM countries WHERE code = 'ITA' OR code = 'SMR' OR code = 'VAT';
+-- So Carmen is either in Italy or San Marino. Now we need to check if either of those speak any other language besides Italian.
+
+SELECT * FROM countrylanguages WHERE countrycode = 'ITA' OR countrycode = 'SMR';
+-- Carmen must be in San Marino since Italian is the only language that they speak there.
+
+--------------------------------------------------------------------------------------------
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
