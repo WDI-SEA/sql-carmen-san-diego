0ab82f09d8adcc4caf8602466b437e7d455a5dca
Kenny Jean <kennyjean561@gmail.com>
2021-01-08 10:58:42 -0600

    working on clues

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,13 +2,15 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-
+SELECT region, population, localname FROM countries WHERE region = 'Southern Europe';
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+SELECT * FROM countrylanguages;
 
+SELECT * FROM countries WHERE localname='Santa Sede/Citt� del Vaticano';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
