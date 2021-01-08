1f2bb265aa8640b7e6dcc55d467e81f2bbae4122
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-01-08 12:57:52 -0600

    Got It

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,12 +1,56 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
+ population |              name               
+------------+---------------------------------
+   57680000 | Italy
+   39441700 | Spain
+   10640000 | Yugoslavia
+   10545700 | Greece
+    9997600 | Portugal
+    4473000 | Croatia
+    3972000 | Bosnia and Herzegovina
+    3401200 | Albania
+    2024000 | Macedonia
+    1987800 | Slovenia
+     380200 | Malta
+      78000 | Andorra
+      27000 | San Marino
+      25000 | Gibraltar
+       1000 | Holy See (Vatican Cities State)
+(15 rows)
 
+Holy See
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
+world=# SELECT code, name FROM countries WHERE region='Southern Europe';
+ code |              name               
+------+---------------------------------
+ ALB  | Albania
+ AND  | Andorra
+ BIH  | Bosnia and Herzegovina
+ ESP  | Spain
+ GIB  | Gibraltar
+ ITA  | Italy
+ YUG  | Yugoslavia
+ GRC  | Greece
+ HRV  | Croatia
+ MKD  | Macedonia
+ MLT  | Malta
+ PRT  | Portugal
+ SMR  | San Marino
+ SVN  | Slovenia
+ VAT  | Holy See (Vatican Cities State)
+(15 rows)
+
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ VAT         | Italian  | t          |          0
+(1 row)
+
 
 
 
@@ -14,25 +58,45 @@
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ ITA         | Italian  | t          |       94.1
+ SMR         | Italian  | t          |        100
+ VAT         | Italian  | t          |          0
+ ARG         | Italian  | f          |        1.7
+ AUS         | Italian  | f          |        2.2
+ LIE         | Italian  | f          |        2.5
+ BEL         | Italian  | f          |        2.4
+ BRA         | Italian  | f          |        0.4
+ LUX         | Italian  | f          |        4.6
+ MCO         | Italian  | f          |       16.1
+ CHE         | Italian  | t          |        7.7
+ CAN         | Italian  | f          |        1.7
+ FRA         | Italian  | f          |        0.4
+ DEU         | Italian  | f          |        0.7
+ USA         | Italian  | f          |        0.6
+(15 rows)
+  SMR
+
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
-
+3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+Brazil, Serra
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
-
+Brasilia
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
@@ -53,4 +117,4 @@
 
 
 
--- She's in ____________________________!
+She's in ____SANTA MONICA, CA________________________!
