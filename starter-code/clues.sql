f455f3b94cc0b8445315851578129dd8b7f46832
Blake DeGraw <bbdegraw@gmail.com>
2021-01-07 21:16:53 -0800

    clue 6 done

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -45,13 +45,15 @@
 -- ON countries.code = cities.countrycode
 -- ORDER BY countries.continent;
 
--- Serra
+-- Serra, Brazil
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+-- SELECT * FROM countries ORDER BY id;
 
+--Brasilia
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
