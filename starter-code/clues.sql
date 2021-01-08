8dd67a842cac8dc9d9dbf0df0c1e87d705c57b99
Your Name <you@example.com>
2021-01-07 21:01:46 -0800

    Figured out where Carmen San Diego is! Figured out the last clue, clue 7.

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -40,23 +40,46 @@ SELECT * FROM countrylanguages WHERE countrycode = 'ITA' OR countrycode = 'SMR';
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+SELECT * from cities WHERE countrycode = 'SMR';
+-- Carmen must be in Serravalle, San Marino.
 
-
+---------------------------------------------------------------------------------------------
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+-- cities has countrycode
+-- cities has name LIKE 'Ser%'
+-- countries has code 
+-- countries has region = 'South America'
 
+SELECT cities.countrycode, cities.name 
+FROM cities INNER JOIN countries
+ON cities.countrycode = countries.code
+WHERE countries.region = 'South America' AND cities.name LIKE 'Ser%';
+-- Carmen San Diego was headed to Serra, BRA.
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+countries.code = BRA
+countries.capital
 
+SELECT name, capital FROM countries WHERE code = 'BRA';
+SELECT * FROM cities WHERE id = '211';
+-- She's heading to Brasilia.
 
+-- Can refactor the above into one line of code:
+-- countries.capital = cities.id
+
+SELECT countries.code, cities.id, cities.name
+FROM countries INNER JOIN cities
+ON countries.capital = cities.id
+WHERE countries.code = 'BRA';
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
- -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
- -- landing dock.
+-- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
+-- landing dock.
 -- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
 
@@ -67,13 +90,15 @@ SELECT * FROM countrylanguages WHERE countrycode = 'ITA' OR countrycode = 'SMR';
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
 
+-- population = 91084
+SELECT * FROM cities WHERE population = '91084';
+-- Carmen San Diego is headed to Santa Monica, CA, USA!
+
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
-
--- She's in ____________________________!
-
+-- She's in Santa Monica!
 
 -- \l (lowercase L) to view what databases are available
 -- CREATE DATABASE name
