3a75ad2cb82623a84cb83994598013739b4ce78c
Blake DeGraw <bbdegraw@gmail.com>
2021-01-07 21:20:32 -0800

    all done

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -71,6 +71,6 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+-- SELECT * FROM cities ORDER BY population DESC;
 
-
--- She's in ____________________________!
+-- She's in Santa Monica!
