26cec34a7be5f63d8434f3a248a785398afe042b
aimeepham14 <aimee.pham14@gmail.com>
2022-08-31 14:03:44 -0700

    finished carmen sandiego

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -53,4 +53,150 @@
 
 
 
--- She's in ____________________________!
+-- She's in Santa Monica!
+
+world=# \d
+               List of relations
+ Schema |       Name       | Type  |   Owner   
+--------+------------------+-------+-----------
+ public | cities           | table | aimeepham
+ public | countries        | table | aimeepham
+ public | countrylanguages | table | aimeepham
+(3 rows)
+
+world=# SELECT * FROM cities
+world-# ;
+world=# SELECT * FROM countries;
+world=# SELECT * FROM countries WHERE region = 'Southern Europe';
+world=# SELECT * FROM countrylanguages;
+world=# SELECT * FROM countries WHERE region = 'Southern Europe';
+world=# SELECT * FROM countrylanguages;
+world=# SELECT * FROM countrylanguages WHERE countrycode = 'VAT', 'GIB', 'AND', 'SMR';
+ERROR:  syntax error at or near ","
+LINE 1: ... * FROM countrylanguages WHERE countrycode = 'VAT', 'GIB', '...
+                                                             ^
+world=# SELECT * FROM countrylanguages WHERE countrycode = 'VAT';
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ VAT         | Italian  | t          |          0
+(1 row)
+
+world=# SELECT * FROM countrylanguages WHERE countrycode = 'GIB';
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ GIB         | English  | t          |       88.9
+ GIB         | Arabic   | f          |        7.4
+(2 rows)
+
+world=# SELECT * FROM countrylanguages WHERE countrycode = 'AND';
+ countrycode |  language  | isofficial | percentage 
+-------------+------------+------------+------------
+ AND         | Spanish    | f          |       44.6
+ AND         | Catalan    | t          |       32.3
+ AND         | Portuguese | f          |       10.8
+ AND         | French     | f          |        6.2
+(4 rows)
+
+world=# SELECT * FROM countrylanguages WHERE countrycode = 'SMR';
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ SMR         | Italian  | t          |        100
+(1 row)
+
+world=# SELECT * FROM cities
+;
+world=# SELECT * FROM countries;
+world=# SELECT * FROM cities
+;
+world=# SELECT * FROM cities WHERE countrycode = 'SMR';
+  id  |    name    | countrycode |     district      | population 
+------+------------+-------------+-------------------+------------
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+ 3171 | San Marino | SMR         | San Marino        |       2294
+(2 rows)
+
+world=# SELECT * FROM countries;                       
+world=# SELECT * FROM countries WHERE localname = 'Serravalle'
+world-# SELECT * FROM countries WHERE region = 'South America'
+world-# ;
+ERROR:  syntax error at or near "SELECT"
+LINE 2: SELECT * FROM countries WHERE region = 'South America'
+        ^
+world=# SELECT * FROM countries WHERE localname = 'Serravalle'
+SELECT * FROM countries WHERE region = 'South America'
+;
+ERROR:  syntax error at or near "SELECT"
+LINE 2: SELECT * FROM countries WHERE region = 'South America'
+        ^
+world=# SELECT * FROM countries WHERE region = 'South America';
+world=# SELECT * FROM cities;
+world=# SELECT * FROM cities WHERE name LIKE {Serr};
+ERROR:  syntax error at or near "{"
+LINE 1: SELECT * FROM cities WHERE name LIKE {Serr};
+                                             ^
+world=# SELECT * FROM cities WHERE name LIKE Serr;
+ERROR:  column "serr" does not exist
+LINE 1: SELECT * FROM cities WHERE name LIKE Serr;
+                                             ^
+world=# SELECT * FROM cities WHERE name LIKE 'Serr';
+ id | name | countrycode | district | population 
+----+------+-------------+----------+------------
+(0 rows)
+
+world=# SELECT * FROM cities WHERE name LIKE 'serr';
+ id | name | countrycode | district | population 
+----+------+-------------+----------+------------
+(0 rows)
+
+world=# SELECT * FROM cities WHERE name LIKE 'san';
+ id | name | countrycode | district | population 
+----+------+-------------+----------+------------
+(0 rows)
+
+world=# SELECT * FROM cities WHERE name = 'serr';
+ id | name | countrycode | district | population 
+----+------+-------------+----------+------------
+(0 rows)
+
+world=# SELECT * FROM cities WHERE name = 'san';
+ id | name | countrycode | district | population 
+----+------+-------------+----------+------------
+(0 rows)
+
+world=# SELECT * FROM cities WHERE name = 'valle';
+ id | name | countrycode | district | population 
+----+------+-------------+----------+------------
+(0 rows)
+
+world=# SELECT * FROM cities WHERE NAME LIKE 'Serr';
+ id | name | countrycode | district | population 
+----+------+-------------+----------+------------
+(0 rows)
+
+world=# SELECT * FROM cities WHERE NAME LIKE 'Serr%';
+  id  |    name    | countrycode |     district      | population 
+------+------------+-------------+-------------------+------------
+  265 | Serra      | BRA         | Esp�rito Santo    |     302666
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+(2 rows)
+
+world=# SELECT * FROM countries
+world-# ;
+world=# SELECT * FROM cities
+world-# ;
+world=# SELECT * FROM cities
+;
+world=# SELECT * FROM cities       
+;
+world=# SELECT * FROM cities WHERE name = 'Bras�lia';
+ id  |   name   | countrycode |     district     | population 
+-----+----------+-------------+------------------+------------
+ 211 | Bras�lia | BRA         | Distrito Federal |    1969868
+(1 row)
+
+world=# SELECT * FROM cities WHERE population = '91084'
+world-# ;
+  id  |     name     | countrycode |  district  | population 
+------+--------------+-------------+------------+------------
+ 4060 | Santa Monica | USA         | California |      91084
+(1 row)
