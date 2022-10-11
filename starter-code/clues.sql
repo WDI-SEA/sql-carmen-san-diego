ccd37e1273f6bb3fef3098156278ea560fda2c17
Fuad AlShuroogi <fmjs@Fuads-MacBook-Air.local>
2022-10-12 00:17:53 +0300

    Fuad AlShuroogi - Have found where Carmen is in

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,18 +2,28 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+select code,name,localname,region, min(population) as least_populated_country from countries
+where region like '%Southern Europe%'
+group by code,name,localname,region,population
+order by population
+fetch first row only
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+select * from countrylanguages
+where countrycode = 'VAT'
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+select * from countries 
+where region like '%Southern Europe%'
+and name like '%Ita%'
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
@@ -21,12 +31,23 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+ select name,min(population) as least_populated_country from cities
+where countrycode = 'ITA'
+group by name , population
+order by population
+fetch first row only
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+select * from cities
+where name like '%Pes%'
+fetch first row only
+
+select * from countries
+where code = 'BRA'
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -34,6 +55,8 @@
  -- follow right behind you!
 
 
+select name from countries
+where capital = '211'
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -50,7 +73,9 @@
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
-
+select name from cities
+where population = '91084'
 
 
 -- She's in ____________________________!
+She's in Santa Monica ! 
