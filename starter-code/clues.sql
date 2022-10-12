6133a78671a13f3628a602ac8145eb406018689f
Ameera Ali <amiraabdullamir@gmail.com>
2022-10-12 19:34:23 +0300

    task completed

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,39 +1,41 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
-select name, population
-from countries
-WHERE region='Southern Europe'
-ORDER BY population
+        select name, population
+        from countries
+        WHERE region='Southern Europe'
+        ORDER BY population
 
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-select language from countrylanguages WHERE countrycode='VAT'
+    select language from countrylanguages WHERE countrycode='VAT'
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
-
+ select  countrycode,language
+ From countrylanguages 
+ WHERE language ='Italian'
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
-select * FROM cities
-where countrycode='SMR'
+    select * FROM cities
+    where countrycode='SMR'
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-select * FROM cities
-where name Like '%Serra%';
+    select * FROM cities
+    where name Like '%Serra%';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
@@ -55,7 +57,9 @@ where name Like '%Serra%';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+    select name,population from cities
+    Where population BETWEEN 91000 AND 91999
+    order by population
 
 
-
--- She's in ____________________________!
+-- She's in _______Santa Monica_____________________!
